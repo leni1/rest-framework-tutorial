08e43ed4aea75d2526a9f8fac8cff9167d91cc44
Leni Kadali Mutungi <lenikmutungi@gmail.com>
2019-10-16 16:46:33 +0300

    ch: Fix indentation error

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -44,7 +44,7 @@ class SnippetDetail(APIView):
         if serializer.is_valid():
                 serializer.save()
                 return Response(serializer.data)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
         def delete(self, request, pk, format=None):
             snippet.delete()
